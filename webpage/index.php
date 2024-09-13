aef860d1d3f462539a2f8ff182062a8676b573f8
BeatrizJVC <117328913+BeatrizJVC@users.noreply.github.com>
2024-09-13 16:42:30 -0300

    Create index.php

---
+++ webpage/index.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Modo Sano</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=yes"
+        />
+
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+        <link rel="shortcut icon" href="img/invertido.png">
+    </head>
+        
+    <body>
+    <section class="vh-100" style="background-color: #212f3c;">
+
+    <div class="container d-flex flex-column justify-content-center align-items-center" style="height: 100vh;" data-aos="zoom-in">
+        <h1 class="mb-4" style="color: #FFFFFF;">Ingrese a su cuenta</h1>
+        <div>
+            <button type="button" class="btn btn-primary btn-lg" onclick="window.location.href='secciones/login_paciente.php'">Soy paciente</button>
+            <button type="button" class="btn btn-secondary btn-lg" onclick="window.location.href='secciones/login_especialista.php'">Soy especialista</button>
+            <button type="button" class="btn btn-info btn-lg" style="color: #FFFFFF;" onclick="window.location.href='secciones/signup.php'">No tengo cuenta</button>
+        </div>
+    </div>
+        <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"
+        ></script>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossorigin="anonymous"
+        ></script>
+        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+        <script>
+            AOS.init();
+        </script>
+        </section>
+    </body>
+</html>
