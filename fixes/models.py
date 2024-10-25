a70ed712d30f311046f34e7ffdd10214ccac28fb
javiera paz <131200387+j4vin@users.noreply.github.com>
2024-10-25 16:18:50 -0300

    fixed models.py

---
+++ fixes/models.py
@@ -0,0 +1,30 @@
+import hashlib
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+db = SQLAlchemy()
+
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    email_hash = db.Column(db.String(150), unique=True, nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
+    role = db.Column(db.String(50), nullable=False)  # 'specialist', 'patient', 'admin'
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def set_email(self, email):
+        # Asegurar que la cadena esté codificada en UTF-8 antes de hashear
+        self.email_hash = hashlib.sha256(email.encode('utf-8')).hexdigest()
+
+    def check_email(self, email):
+        # Asegurar que la cadena esté codificada en UTF-8 antes de hashear
+        email_hashed = hashlib.sha256(email.encode('utf-8')).hexdigest()
+        return email_hashed == self.email_hash
